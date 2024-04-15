d644b4ffdb3ad1668d5022b538fc7d2eae6a6456
김지호 <jiho001108@gmail.com>
2024-04-15 10:32:07 +0900

    특정 값이 업데이트될 때만 실행하고 싶을 때

--- src/Info.js
+++ src/Info.js
@@ -3,13 +3,11 @@ import { useState, useEffect } from "react";
 const Info = () => {
 	const [name, setName] = useState("");
 	const [nickname, setNickname] = useState("");
+
 	useEffect(() => {
-		console.log("렌더링이 완료되었습니다!");
-		console.log({
-			name,
-			nickname,
-		});
-	}, []);
+		console.log(name);
+	}, [name]);
+
 	const onChangeName = (e) => {
 		setName(e.target.value);
 	};
