9f152464e497d50b881d6ef1b8ca812b362e1ca8
김지호 <jiho001108@gmail.com>
2024-04-15 11:00:47 +0900

    설명 추가

--- src/Info.js
+++ src/Info.js
@@ -4,6 +4,11 @@ const Info = () => {
 	const [name, setName] = useState("");
 	const [nickname, setNickname] = useState("");
 
+	/**
+	 * 배열 존재: 마운트 된 후 실행
+	 * 배열에 값 존재: 값이 업데이트 된 후 실행
+	 * 리턴값 존재: 언마운트 직전 and 업데이트 직전에 실행
+	 */
 	useEffect(() => {
 		console.log("effect");
 		console.log(name);
