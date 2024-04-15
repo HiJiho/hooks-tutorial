4fd78002d72550f00133d5a109c605d71216a2ae
김지호 <jiho001108@gmail.com>
2024-04-15 21:02:41 +0900

    8.3 useReducer 완료

--- src/Info.js
+++ src/Info.js
@@ -1,36 +1,27 @@
-import { useState, useEffect } from "react";
+import { useReducer } from "react";
+
+function reducer(state, action) {
+	return {
+		...state,
+		[action.name]: action.value,
+	};
+}
 
 const Info = () => {
-	const [name, setName] = useState("");
-	const [nickname, setNickname] = useState("");
-
-	/**
-	 * 배열 존재: 마운트 된 후 실행
-	 * 배열에 값 존재: 값이 업데이트 된 후 실행
-	 * 리턴값 존재: 언마운트 직전 and 업데이트 직전에 실행
-	 */
-	useEffect(() => {
-		console.log("effect");
-		console.log(name);
-		return () => {
-			console.log("cleanup");
-			console.log(name);
-		};
-	}, [name]);
-
-	const onChangeName = (e) => {
-		setName(e.target.value);
-	};
-
-	const onChangeNickname = (e) => {
-		setNickname(e.target.value);
+	const [state, dispatch] = useReducer(reducer, {
+		name: "",
+		nickname: "",
+	});
+	const { name, nickname } = state;
+	const onChange = (e) => {
+		dispatch(e.target);
 	};
 
 	return (
 		<div>
 			<div>
-				<input value={name} onChange={onChangeName} />
-				<input value={nickname} onChange={onChangeNickname} />
+				<input name="name" value={name} onChange={onChange} />
+				<input name="nickname" value={nickname} onChange={onChange} />
 			</div>
 			<div>
 				<b>이름:</b> {name}
